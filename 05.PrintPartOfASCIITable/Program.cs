9b98ad1f580e6e40455a1782d4c63bec5bf8d335
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-05-25 17:12:41 +0300

    Задачи 5 и 6.

---
+++ 05.PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * 05.PrintPartOfASCIITable
+ */
+
+namespace _05.PrintPartOfASCIITable;
+
+internal class Program
+{
+
+    static void Main(string[] _)
+    {
+
+        int startCode = int.Parse(Console.ReadLine()!);
+        int endCode = int.Parse(Console.ReadLine()!);
+
+        while (startCode <= endCode)
+        {
+
+            Console.Write((char)startCode++ + " ");
+
+        }
+
+    }
+
+}
---
+++ 06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+
+/**
+ * 1.	Triples of Latin Letters
+ */
+
+namespace _06.TriplesOfLatinLetters;
+
+internal class Program
+{
+
+    static void Main(string[] _)
+    {
+
+        int letterCount = int.Parse(Console.ReadLine()!);
+
+        for (int i = 0; i < letterCount; ++i)
+        {
+
+            for (int j = 0; j < 3; j++)
+            {
+
+                for (int k = 0; k < 3; k++)
+                {
+
+                    char firstLetter = (char)('a' + i);
+                    char secondLetter = (char)('a' + j);
+                    char thirdLetter = (char)('a' + k);
+                    Console.WriteLine($"{firstLetter}{secondLetter}{thirdLetter}");
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
