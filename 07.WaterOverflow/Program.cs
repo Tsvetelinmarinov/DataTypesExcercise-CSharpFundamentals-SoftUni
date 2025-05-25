e9132a5c9b6eeaccdc63e244b3f6ee92619b0628
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-05-25 17:25:29 +0300

    Задача 7.

---
+++ 07.WaterOverflow/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Diagnostics;
+
+/**
+ * 07. Water Overflow
+ */
+namespace _07.WaterOverflow;
+
+internal class Program
+{
+
+    static void Main(string[] _)
+    {
+
+        int count = int.Parse(Console.ReadLine()!);
+        int total = default;
+        for (int i = 0; i < count; i++)
+        {
+
+            int liters = int.Parse(Console.ReadLine()!);
+            if (total + liters >= 255)
+            {
+                Console.WriteLine("Insufficient capacity!");
+                continue;
+            }
+            else if (total + liters <= 255)
+            {
+                total += liters;
+            }
+
+        }
+
+        Console.WriteLine(total);
+
+    }
+
+}
